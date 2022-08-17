0551efdaef5e72891d2a1673ee8ecf31fa479fcb
igate solutions <cbatch42@gmail.com>
2022-08-17 20:38:22 +0530

    v1

---
+++ prog86.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	if(10>20)
+		printf("one\n");
+	else if(40>20)
+		printf("two\n");
+	else if(20<=20)
+		printf("three\n");
+	else if(60==60)
+		printf("four\n");
+	return 0;
+}
+
